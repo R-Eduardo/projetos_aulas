4334c740433804a4f8397be39d1bc9bc10196941
R-Eduardo <raimundo_eduardo@outlook.com>
2024-12-18 10:55:45 -0300

    add_aula_2

---
+++ Python/aula02_separadores.py
@@ -0,0 +1,10 @@
+# CRLF  |  \r\n
+# LF    |  \n
+
+print(1, 2, sep="-", end='\n##')
+print(1, 2, sep="-", end='#')
+print(1, 2, sep='-', end='\n')
+
+# Separadores sep="-"
+# Modificadores de finais end='#'
+
